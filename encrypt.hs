953cefbb8279afbfd6dfc1c3257e727d86003701
Vincent Hanquez <vincent@snarc.org>
2015-10-12 17:34:22 +0100

    add old encrypt program

---
+++ encrypt.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- an old encryption program. do not use anymore.
+module Main where
+
+import qualified Crypto.KDF.PBKDF2 as PBKDF2
+import Crypto.Cipher.AES
+import Crypto.Cipher.Types
+import "cryptonite" Crypto.Hash
+import Crypto.Error
+import Control.Applicative
+import Control.Exception
+import System.IO
+import System.Environment
+import qualified Data.ByteString as B
+import Options.Applicative
+import qualified Data.ByteString.UTF8 as UTF8
+import qualified Data.ByteArray as BA
+
+askPasswd :: IO String
+askPasswd = bracket
+    (hSetEcho stdin False)
+    (\() -> hSetEcho stdin True)
+    (\() -> hGetLine stdin)
+
+getKey :: IO AES256
+getKey = do
+    passwd <- UTF8.fromString <$> askPasswd
+    return $ throwCryptoError $ cipherInit $ stretched passwd
+  where
+    stretched :: B.ByteString -> B.ByteString
+    stretched passwd = PBKDF2.generate (PBKDF2.prfHMAC SHA512) (PBKDF2.Parameters 4096 32) passwd salt
+
+salt :: B.ByteString
+salt = "ctrfile"
+
+doCtr key iv hIn hOut = loop iv
+  where loop iv = do
+            content <- B.hGet hIn 1024
+            B.hPut hOut $ ctrCombine key iv content
+            if B.length content < 1024
+                then return ()
+                else loop (ivAdd iv (1024 `div` 16))
+
+doDecrypt file key =
+    withFile file ReadMode $ \h ->
+    withFile (file ++ ".decrypt") WriteMode $ \hout -> do
+        iv <- maybe (error "cannot get IV") id . makeIV <$> B.hGet h 16
+        doCtr key iv h hout
+
+
+doEncrypt file key = do
+    iv <- withFile "/dev/urandom" ReadMode $ \h -> (maybe (error "cannot get IV") id . makeIV <$> B.hGet h 16)
+    withFile file ReadMode $ \h ->
+        withFile (file ++ ".encrypt") WriteMode $ \hout -> do
+            B.hPut hout $ BA.convert iv
+            doCtr key iv h hout
+
+main = do
+    args <- getArgs
+    case args of
+        "decrypt":file:[] -> getKey >>= doDecrypt file
+        "encrypt":file:[] -> getKey >>= doEncrypt file
+        _                 -> error "unknown command"
