e9eb7b1490f630c66bd68b8e0e8be41b12cfc113
Vincent Hanquez <vincent@snarc.org>
2016-02-23 08:36:06 +0000

    straighten tests

---
+++ tests/HashfsState.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE FlexibleInstances #-}
+module HashfsState where
+
+import           Control.Applicative
+import           Control.Monad
+
+import           Test.Tasty
+import           Test.Tasty.QuickCheck
+import           Test.QuickCheck.Monadic
+
+import           Storage.HashFS.Meta
+import           Storage.HashFS
+import           System.Directory
+import           Crypto.Hash
+import           Data.List
+
+import           Data.Hourglass
+import           Data.Word
+
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Map as M
+
+data State h = State
+    { stateContents   :: [Digest h]
+    , stateTags       :: [Tag]
+    , stateAssociates :: M.Map (Digest h) (DataInfo, [Tag])
+    } deriving (Show,Eq)
+
+instance HashAlgorithm h => Arbitrary (State h) where
+    arbitrary = do
+        digests <- choose (1,351) >>= flip replicateM (hash . BC.pack <$> replicateM 8 arbitrary)
+        tags    <- choose (1,124) >>= flip replicateM arbitrary
+        ass     <- forM digests $ \dg -> do
+                    di     <- arbitrary
+                    dgTags <- nub <$> (choose (0,4) >>= flip replicateM (elements tags))
+                    return (dg, (di, dgTags))
+                    
+        return $ State digests tags (M.fromList ass)
+
+newState :: HashAlgorithm h => h -> Gen (State h)
+newState _ = arbitrary
+
+instance Arbitrary Tag where
+    arbitrary =
+        Tag <$> elements [Nothing, Just Other, Just Person, Just Location, Just Group]
+            <*> elements arbitraryTag
+
+forEachDigestsAndData :: HashAlgorithm h => State h -> ((Digest h, (DataInfo, [Tag])) -> IO a) -> IO ()
+forEachDigestsAndData st f = forM_ (M.toList $ stateAssociates st) f
+
+forEachTags :: HashAlgorithm h => State h -> (Tag -> IO a) -> IO ()
+forEachTags st f = forM_ (stateTags st) f
+
+forEachDigests :: HashAlgorithm h => State h -> (Digest h -> IO a) -> IO ()
+forEachDigests st f = forM_ (stateContents st) f
+
+stateGetUntagged :: State h -> [Digest h]
+stateGetUntagged st =
+    map fst $ filter (\(_,(_,ts)) -> null ts) $ M.toList $ stateAssociates st
+
+stateGetTagOf :: State h -> Digest h -> [Tag]
+stateGetTagOf st dg =
+    maybe [] snd $ M.lookup dg $ stateAssociates st
+
+stateGetDigestTaggedWith :: State h -> Tag -> [Digest h]
+stateGetDigestTaggedWith st tag =
+    map fst $ filter (\(_,(_,ts)) -> tag `elem` ts) $ M.toList $ stateAssociates st
+
+arbitraryFileContent :: [String]
+arbitraryFileContent = [ [x,y] | x <- ['a'..'b'], y <- ['a'..'z'] ]
+
+arbitraryDigest :: HashAlgorithm h => h -> [Digest h]
+arbitraryDigest hashAlg = map (hashWith hashAlg . BC.pack) arbitraryFileContent
+
+arbitraryTag :: [String]
+arbitraryTag =
+    ["foo","bar","baz","time","eol","a","b","c", "a'b'c", "he huk neh kak jajvak" ]
+    ++ map (("tag-" ++) . show) [15..124 :: Int]
+    ++ map (("group-" ++) . show) [14..84 :: Int]
+
+arbitraryFilesize :: [Word64]
+arbitraryFilesize = [1, 1248908, 4129124, 2901, 24180421490, 12389, 4125]
+
+arbitraryDataInfo :: [DataInfo]
+arbitraryDataInfo =
+    [ DataInfo sz dt Nothing filename
+    | filename <- [Nothing, Just "abc.jpg", Just "xoxo"]
+    , dt       <- map (Just . toElapsed) [1, 2901, 8249414]
+    , sz       <- arbitraryFilesize
+    ]
+
+instance Arbitrary DataInfo where
+    arbitrary = do
+        filename <- elements [Nothing, Just "abc.jpg", Just "xoxo"]
+        dt       <- (Just . toElapsed) <$> elements [1, 2901, 8249414]
+        sz       <- choose (1, maximum arbitraryFilesize + 20)
+        return $ DataInfo sz dt Nothing filename
+
+toElapsed = Elapsed . Seconds
+
+data Query = Query
+    { queryString :: String
+    , queryFilter :: DataInfo -> [Tag] -> Bool
+    }
+
+data QueryType = QueryFilesize | QueryRating
+    deriving (Show,Eq,Enum,Bounded)
+
+instance Arbitrary Query where
+    arbitrary = do
+        ty <- elements [minBound..] 
+        case ty of
+            QueryFilesize -> do
+                (str, fct) <- genNumQuery $ fromIntegral (maximum arbitraryFilesize + 1)
+                return $ Query { queryString = "filesize " ++ str
+                               , queryFilter = \dt _ -> fct . fromIntegral . dataSize $ dt
+                               }
+            QueryRating -> do
+                -- FIXME using dataSize .. as DataInfo doesn't have rating
+                (str, fct) <- genNumQuery 5
+                return $ Query { queryString = "rating " ++ str
+                               , queryFilter = \dt _ -> fct . fromIntegral . dataSize $ dt
+                               }
+
+-- :: [Query] -> 
+            
+genNumQuery :: Integer -> Gen (String, Integer -> Bool)
+genNumQuery maxN = do
+    op       <- elements [GT,EQ,LT]
+    included <- elements [False,True]
+    n        <- choose (1, maxN)
+    let (opStr, fct) = case (op, included) of
+                    (GT, True)  -> (">=", (>=) n)
+                    (GT, False) -> (">", (>) n)
+                    (LT, True)  -> ("<=", (<=) n)
+                    (LT, False) -> ("<", (<) n)
+                    (EQ, _)     -> ("==", (==) n)
+    return (opStr ++ " " ++ show n, fct)
--- tests/Sql.hs
+++ tests/Sql.hs
@@ -6,7 +6,7 @@ import           Control.Applicative
 import           Control.Monad
 
 import           Test.Tasty
-import           Test.Tasty.QuickCheck
+import           Test.Tasty.QuickCheck hiding (shuffle)
 import           Test.QuickCheck.Monadic
 
 import           Storage.HashFS.Meta
@@ -18,21 +18,7 @@ import           Data.Hourglass
 
 import qualified Data.ByteString.Char8 as BC
 
-data Infinite a = Infinite a (Infinite a)
-
-grab :: Infinite a -> (a, Infinite a)
-grab (Infinite a next) = (a, next)
-
-rng :: Int -- initial N
-    -> Int -- tweak
-    -> Int -- mod
-    -> Infinite Int
-rng initN tweak m = loop (initN * tweak)
-  where
-    (a,b) = (2901,643)
-    loop n =
-        let nextN = (a * n + b) `mod` m
-         in Infinite nextN $ loop (nextN * tweak)
+import           HashfsState
 
 newtype SqlFile = SqlFile String
     deriving (Show,Eq,Read)
@@ -56,100 +42,48 @@ instance Arbitrary SqlHash where
                          , SqlHash SHA512
                          ]
 
-withParameters :: (forall h . HashAlgorithm h => h -> SqlFile -> Int -> IO ()) -> PropertyM IO ()
+withParameters :: (forall h . HashAlgorithm h => SqlFile -> State h -> IO ()) -> PropertyM IO ()
 withParameters f = do
-    (SqlHash h, sqlfile@(SqlFile {}), sqlShuffle) <- pick $ (,,) <$> arbitrary <*> arbitrary <*> arbitrary
+    --(SqlHash h, sqlfile@(SqlFile {}), sqlSt) <- pick $ (,,) <$> arbitrary <*> arbitrary <*> arbitrary
+    (SqlHash h, sqlfile@(SqlFile {})) <- pick $ (,) <$> arbitrary <*> arbitrary
+    sqlSt <- pick (newState h)
     run $ do
         createDirectoryIfMissing True testDirPrefix
-        f h sqlfile sqlShuffle
+        f sqlfile sqlSt
     return ()
 
-arbitraryFileContent :: [String]
-arbitraryFileContent = [ [x,y] | x <- ['a'..'b'], y <- ['a'..'z'] ]
-
-arbitraryDigest :: HashAlgorithm h => h -> [Digest h]
-arbitraryDigest hashAlg = map (hashWith hashAlg . BC.pack) arbitraryFileContent
-
-arbitraryTag :: [Tag]
-arbitraryTag = map (Tag (Just Other)) l
-  where
-    l = ["foo","bar","baz","time","eol","a","b","c"]
-        ++ map (("tag-" ++) . show) [15..124 :: Int]
-        ++ map (("group-" ++) . show) [14..84 :: Int]
-
-arbitraryDataInfo :: [DataInfo]
-arbitraryDataInfo =
-    [ DataInfo sz dt Nothing filename
-    | filename <- [Nothing, Just "abc.jpg", Just "xoxo"]
-    , dt       <- map (Just . toElapsed) [1, 2901, 8249414]
-    , sz       <- [1, 1248908, 2901]
-    ]
-  where
-    toElapsed = Elapsed . Seconds
-
-associateTags :: Int -> [Digest h] -> [Tag] -> [(Digest h, DataInfo, [Tag])]
-associateTags shuffle ds ts =
-    combine (rng shuffle 3 (length tagList)) (shuffleList (rng shuffle 7 124914) ds)
-  where
-    tagList = shuffleList (rng shuffle 19 24195) ts
-
-    combine _ []     = []
-    combine r (d:ds) =
-        let (n, r') = grab r
-            nbItems = n `mod` 5
-            x = n `mod` (length tagList - nbItems)
-            (_, tags2) = splitAt x tagList
-            di = elemN n arbitraryDataInfo
-         in (d, di, take nbItems tags2) : combine r' ds
-
-    elemN n l = l !! (n `mod` length l)
-
-    shuffleList rng l = loop rng [] l
-      where
-        loop _ acc []  = acc
-        loop _ acc [x] = x : acc
-        loop r acc l   =
-            let (n, r') = grab r
-             in case splitAt (n `mod` len) l of
-                ([], [])   -> acc
-                (x:l1, []) -> loop r' (x : acc) l1
-                (l1, x:l2) -> loop r' (x : acc) (l1 ++ l2)
-        len = length l
-
-sqlQueriesWork :: HashAlgorithm h => h -> SqlFile -> Int -> IO ()
-sqlQueriesWork hashAlg (SqlFile f) shuffle = do
+--sqlQueriesWork :: HashAlgorithm h => h -> SqlFile -> State h -> IO ()
+sqlQueriesWork :: HashAlgorithm h => SqlFile -> State h -> IO ()
+sqlQueriesWork (SqlFile f) st = do
     conn <- either error id <$> metaConnect "sqlite3" f
-    let dgs = arbitraryDigest hashAlg
 
-    let ass = associateTags shuffle dgs arbitraryTag
-    forM_ ass $ \(dg, di, _) -> do
-        metaAddData conn dg di
-    forM_ arbitraryTag $ \t -> do
-        metaCreateTag conn t
+    -- create data in the database
+    forEachDigestsAndData st $ \(dg, (di, _)) -> metaAddData conn dg di
+    forEachTags st $ \t -> metaCreateTag conn t
     metaCommit conn
 
-    forM_ ass $ \(d, _, ts) -> do
-        mapM_ (metaTag conn d) ts
+    -- tag data
+    forEachDigestsAndData st $ \(dg, (_, ts)) -> mapM_ (metaTag conn dg) ts
     metaCommit conn
 
-    forM_ arbitraryTag $ \tag -> do
+    -- check that for each tag, we find the right set of data
+    forEachTags st $ \tag -> do
         found <- metaTagGetDigests conn tag
-        let expected = map fst3 $ filter (\(_,_,ts) -> tag `elem` ts) ass
+        let expected = stateGetDigestTaggedWith st tag 
         listCompare ("digest for " ++ show tag ++ " missing ") expected found
 
-    forM_ dgs $ \dg -> do
+    -- check that for each digests, we find the right set of tags
+    forEachDigests st $ \dg -> do
         found <- metaDigestGetTags conn dg
-        let expected = maybe [] thd3 $ find (\(d,_,_) -> d == dg) ass
+        let expected = stateGetTagOf st dg
         listCompare ("tags for digest " ++ show dg ++ " missing") expected found
 
     notTagged <- metaFindDigestsNotTagged conn
-    let expected = map fst3 $ filter (\(_,_,ts) -> null ts) ass
+    let expected = stateGetUntagged st
     listCompare ("not tagged missing") expected notTagged
 
     return ()
   where
-    fst3 (a,_,_) = a
-    thd3 (_,_,c) = c
     listCompare err expected found
         | sort found /= sort expected = error (err ++ "\ngot     : " ++ show found ++ "\nexpected: " ++ show expected)
         | otherwise = return ()
